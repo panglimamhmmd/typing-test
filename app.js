da507c450e04e6ea1ee96f343c4097e4ec769d56
fattyPunk <yahyakoto@gmail.com>
2023-02-21 21:07:32 +0700

    saving

--- app.js
+++ app.js
@@ -1,29 +1,27 @@
 let duration = 0;
 if (!sessionStorage.getItem("duration")) {
   duration = 60; // defalt duration
-  console.log("test");
 } else {
   duration = sessionStorage.getItem("duration");
-  console.log(duration);
 }
 
 const timer = document.getElementById("timer");
 const userInput = document.getElementById("typing_area");
 const wordsValid = document.getElementById("wordsValid");
 const wordsContainer = document.getElementById("words_container");
-const words =
-  "dan yang di tidak untuk dari dengan pada dalam itu saya ini adalah kamu kita bisa apa sudah jika atau mereka harus ke ada juga tapi akan atas karena baru dia lagi punya hanya mengapa bisa ya bagaimana saat sekarang pada semua mungkin sedang memang jadi benar harusnya banyak kalau sudah seperti tetapi ada sendiri terus jangan kamu dari pun sebelum masih untuk saja tapi tidak akan hari dalam tahu lalu sesuai karena sangat terlalu juga terbaik menurut pernah terjadi di namun belakang setiap lagi bahkan depan sini nanti sebagian mulai harus tiga setelah langsung memang menjadi memungkinkan mengalami secepatnya tinggi waktu menggunakan belajar milik bulan sepertinya awal sangatlah dalamnya sebab kecil paling sering memberikan kesempatan memperbaiki mengetahui mengambil semakin sekarang yang berada pasti bawah dihadapi keluar melalui melihat mengerti bisa dipastikan didalam menambahkan menangani terhadap memperoleh menunjukkan sekaligus tiga hal tanpa tempat waktu menerima kebutuhan tangan membawa menghasilkan sebenarnya meningkat menghadapi langsung mempersiapkan mengejar baru ada ke di yang dari dengan untuk ini tidak aku akan seperti atau mereka kita sudah tapi dalam lain ada ia juga bagi saya namun jadi oleh masih harus bisa kami ataupun sekarang atau jangan sebagai tersebut dapat semua karena hanya tentang saat terlalu dalam banyak telah memiliki sebelum setelah lebih semakin lebih banyak bahwa itu waktu saja sejak sini di mana orang sedang saja bagaimana ada demi lalu terhadap lainnya untuk saat sedangkan baru-baru beberapa melalui masa hidup masing berada selama kemudian kali lagi bagian bahwa mengapa sebelumnya sedikit segala sesuatu sebagian bawah hari selalu bisa jika terlihat sebelumnya berbagai keluar tersebut sekitar mendapatkan sejak akhir jauh melakukan memberikan memiliki sendiri kembali layak karena berarti merupakan sering terus demi terlihat mungkin hal kelompok perlu diberikan pada berkaitan menjadi sudut bertemu yang sebab terhadap walaupun dimana kemungkinan sehingga baru-baru apapun berkaitan masing-masing suatu tentu tinggi besar dalam pendek beberapa memang mampu sistem masing terutama mungkin belakang mudah memasukkan seluruh menunjukkan jelas menjadi sesuai terbuka memungkinkan juga tingkat berarti berbeda khususnya tersedia masih selain sebenarnya melihat berlaku membuat ada luar maka oleh sedikit dalam kecil sepertinya kepada biasa langsung menyediakan tidak sedikitpun sekitar hingga bersama nyata lebih saat itu termasuk banyak kesempatan tentang sebagai yang utama sejak termasuk hal karena berbagai pikiran di kedua sama melihat memilih bertanggungjawab mendapat dilakukan pikiran rencana senang tidak bersama-sama lebih sebagai memiliki setiap tinggi meningkat terus-menerus mendapat rencana seluruh bertindak";
-
-const randomWords = randomizeWords(words.split(" "));
 
 let validWord = 0;
 let spacePressed = 0;
 let isStarted = false;
 let wordWillDisplay = 20;
-let spaceAttemp = 20;
+let spaceAttemp = 20; // display 20 word only
 let typeAttemp = 1;
 let validCharacterTotal = 0;
 let validSpaceTotal = 0;
+const words =
+  "dan yang di tidak untuk dari dengan pada dalam itu saya ini adalah kamu kita bisa apa sudah jika atau mereka harus ke ada juga tapi akan atas karena baru dia lagi punya hanya mengapa bisa ya bagaimana saat sekarang pada semua mungkin sedang memang jadi benar harusnya banyak kalau sudah seperti tetapi ada sendiri terus jangan kamu dari pun sebelum masih untuk saja tapi tidak akan hari dalam tahu lalu sesuai karena sangat terlalu juga terbaik menurut pernah terjadi di namun belakang setiap lagi bahkan depan sini nanti sebagian mulai harus tiga setelah langsung memang menjadi memungkinkan mengalami secepatnya tinggi waktu menggunakan belajar milik bulan sepertinya awal sangatlah dalamnya sebab kecil paling sering memberikan kesempatan memperbaiki mengetahui mengambil semakin sekarang yang berada pasti bawah dihadapi keluar melalui melihat mengerti bisa dipastikan didalam menambahkan menangani terhadap memperoleh menunjukkan sekaligus tiga hal tanpa tempat waktu menerima kebutuhan tangan membawa menghasilkan sebenarnya meningkat menghadapi langsung mempersiapkan mengejar baru ada ke di yang dari dengan untuk ini tidak aku akan seperti atau mereka kita sudah tapi dalam lain ada ia juga bagi saya namun jadi oleh masih harus bisa kami ataupun sekarang atau jangan sebagai tersebut dapat semua karena hanya tentang saat terlalu dalam banyak telah memiliki sebelum setelah lebih semakin lebih banyak bahwa itu waktu saja sejak sini di mana orang sedang saja bagaimana ada demi lalu terhadap lainnya untuk saat sedangkan baru-baru beberapa melalui masa hidup masing berada selama kemudian kali lagi bagian bahwa mengapa sebelumnya sedikit segala sesuatu sebagian bawah hari selalu bisa jika terlihat sebelumnya berbagai keluar tersebut sekitar mendapatkan sejak akhir jauh melakukan memberikan memiliki sendiri kembali layak karena berarti merupakan sering terus demi terlihat mungkin hal kelompok perlu diberikan pada berkaitan menjadi sudut bertemu yang sebab terhadap walaupun dimana kemungkinan sehingga baru-baru apapun berkaitan masing-masing suatu tentu tinggi besar dalam pendek beberapa memang mampu sistem masing terutama mungkin belakang mudah memasukkan seluruh menunjukkan jelas menjadi sesuai terbuka memungkinkan juga tingkat berarti berbeda khususnya tersedia masih selain sebenarnya melihat berlaku membuat ada luar maka oleh sedikit dalam kecil sepertinya kepada biasa langsung menyediakan tidak sedikitpun sekitar hingga bersama nyata lebih saat itu termasuk banyak kesempatan tentang sebagai yang utama sejak termasuk hal karena berbagai pikiran di kedua sama melihat memilih bertanggungjawab mendapat dilakukan pikiran rencana senang tidak bersama-sama lebih sebagai memiliki setiap tinggi meningkat terus-menerus mendapat rencana seluruh bertindak";
+
+const randomWords = randomizeWords(words.split(" "));
 
 window.addEventListener("load", function () {
   wordsBuilder(randomWords, wordsContainer);
@@ -32,40 +30,57 @@ window.addEventListener("load", function () {
   setDurationByDropdown(duration);
 });
 
+userInput.addEventListener("input", function (event) {
+  console.log(event.target.value);
+});
+
 // trigger timer
-userInput.addEventListener("keypress", function (event) {
+userInput.addEventListener("input", function (event) {
   // jika user mulai mengetik,  start timer
-  if (event.key != undefined && !isStarted) {
+  if (event.target.value != undefined && !isStarted) {
     setTimer(duration, timer);
     isStarted = true;
   }
 });
 
-// spasi trigger, agar kata di validasi
-userInput.addEventListener("keypress", function (event) {
-  if (event.key == " ") {
-    if (userInput.value.length > 1) {
-      if (userInput.value[0] == " ") {
-        wordValidating(userInput.value.substr(1), spacePressed);
-      } else {
-        wordValidating(userInput.value, spacePressed);
-      }
-    } else {
-      userInput.value = "";
-      return 0;
-    }
+// spasi trigger, agar kata di validasi (pc)
+// userInput.addEventListener("input", function (event) {
+//   const val = userInput.value;
+//   if (event.key == " ") {
+//     spaceDetected(val);
+//   }
+// });
 
-    spacePressed++;
-    if (spacePressed == spaceAttemp) {
-      typeAttemp++;
-      spaceAttemp += 20;
-      console.log(spaceAttemp);
-      wordsUnhide(wordWillDisplay, typeAttemp);
-      wordsHide(wordWillDisplay, typeAttemp);
-    }
-    userInput.value = "";
+userInput.addEventListener("input", function (event) {
+  const val = userInput.value;
+  const valValid = val.substr(0, val.length - 1);
+  if (event.target.value.includes(" ")) {
+    spaceDetected(valValid);
   }
 });
+
+function spaceDetected(value) {
+  if (value.length > 1) {
+    if (value[0] == " ") {
+      wordValidating(value.substr(1), spacePressed);
+    } else {
+      wordValidating(value, spacePressed);
+    }
+  } else {
+    userInput.value = "";
+    return 0;
+  }
+
+  spacePressed++;
+  if (spacePressed == spaceAttemp) {
+    typeAttemp++;
+    spaceAttemp += 20;
+    console.log(spaceAttemp);
+    wordsUnhide(wordWillDisplay, typeAttemp);
+    wordsHide(wordWillDisplay, typeAttemp);
+  }
+  userInput.value = "";
+}
 
 function randomizeWords(words) {
   // words type = array
@@ -142,7 +157,7 @@ function resultCount() {
   let characterTotal = 0;
 
   wordsContainer.innerHTML = "";
-  console.log(validCharacterTotal, " : ", spacePressed);
+
   randomWords.splice(0, spacePressed).forEach((element) => {
     characterTotal += element.length;
   });
@@ -158,9 +173,9 @@ function resultCount() {
   <h5>WPM: ${wpm}</h5>
   <p>(Words Per Minute)</p>
   <ul class="list-group">
-    <li class="list-group-item">Total Character : (<span class="valid">${correctChar}</span>| <span class="invalid">${
+    <li class="list-group-item">Total Character : ( <span class="valid">${correctChar}</span> | <span class="invalid">${
     characterTotal - correctChar
-  }</span>) ${characterTotal}</li>
+  }</span> ) ${characterTotal} </li>
     <li class="list-group-item">Accuracy : ${accuracy}% </li>
     <li class="list-group-item valid">Correct Words : ${correctWords}</li>
     <li class="list-group-item invalid">Wrong Words : ${incorrectWords}</li>
